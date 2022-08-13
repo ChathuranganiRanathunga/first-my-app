484336d325b58dd231109c0dd283149525119145
Chathurangani Ranathunga <66179809+ChathuranganiRanathunga@users.noreply.github.com>
2022-08-13 22:42:51 +0530

    played with state settings

--- src/App.js
+++ src/App.js
@@ -5,14 +5,16 @@ import Tweet from "./Tweet";
 function App() {
   const [isRed, setRed] = useState(false);
   const [count, setCount] = useState(0);
+
+  const increment = () => {
+    setCount(count + 1);
+    setRed(true);
+  };
+
   return (
     <div className="app">
-      <h1>Hello React!</h1>
-      <Tweet name="Chaa97" message="This is a Random Tweet by Cha." />
-      <Tweet name="Tharushi06" message="This is a Random Tweet by Tharushi." />
-      <Tweet name="Chala06" message="This is a Random Tweet by Chala06." />
-      <Tweet name="Naviya00" message="This is a Random Tweet by Naviya00" />
-      <button>Increment</button>
+      <h1 className={isRed ? "red" : ""}>Change My Color!</h1>
+      <button onClick={increment}>Increment</button>
       <h1>{count}</h1>
     </div>
   );
