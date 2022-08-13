a52a4095b96f76799cc33a4cd3a5f78d33986994
Chathurangani Ranathunga <66179809+ChathuranganiRanathunga@users.noreply.github.com>
2022-08-13 14:43:39 +0530

    Created tweet card :)

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import "./App.css";
 import React from "react";
-import Hello from "./sayHello";
+import Tweet from "./Tweet";
 
 function App() {
   return (
-    <div>
-      <h1>This is the App Component that I first Create</h1>
-      <Hello />
+    <div className="app">
+      <h1>Hello React!</h1>
+      <Tweet />
     </div>
   );
 }
